7dde45b92c421fc4d9c8dcff1cf0a5e9d976ca44
Alex Robertson <hello_im_alex@hotmail.com>
2015-11-02 13:34:53 -0800

    added 2nd viewcontroller for zoomzoom

---
+++ ScrollViewImageGallerys/ZoomViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZoomViewController.h
+//  ScrollViewImageGallerys
+//
+//  Created by Alex on 2015-11-02.
+//  Copyright © 2015 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZoomViewController : UIViewController <UIScrollViewDelegate>
+
+@end
---
+++ ScrollViewImageGallerys/ZoomViewController.m
@@ -0,0 +1,43 @@
+//
+//  ZoomViewController.m
+//  ScrollViewImageGallerys
+//
+//  Created by Alex on 2015-11-02.
+//  Copyright © 2015 Alex. All rights reserved.
+//
+
+#import "ZoomViewController.h"
+
+@interface ZoomViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollViewZoom;
+@property (weak, nonatomic) IBOutlet UIImageView *imageZoom;
+@end
+
+@implementation ZoomViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.imageZoom;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
